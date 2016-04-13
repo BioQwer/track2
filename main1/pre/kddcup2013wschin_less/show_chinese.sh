22c79eae5a1d3b58d9644bcea7929e37d17f4f83
chanhou <iepiechan@gmail.com>
2016-04-14 00:54:21 +0800

    perl utf8 error

--- main1/pre/kddcup2013wschin_less/show_chinese.sh
+++ main1/pre/kddcup2013wschin_less/show_chinese.sh
@@ -3,7 +3,7 @@ CH="information"
 ln -s -T ../../data/Author.csv Author.csv
 
 echo "Split Author.csv..."
-perl split_author.pl Author.csv
+perl -Mutf8 -CS -e split_author.pl Author.csv
 
 echo "Start to detect chinese..."
-perl show_chinese.pl Author.csv.aid Author.csv.name ${CH}/last.all ${CH}/token.all ${CH}/last.ban ${CH}/token.ban|perl show_p.pl > ../../buff/chinese_author.less
+perl -Mutf8 -CS -e show_chinese.pl Author.csv.aid Author.csv.name ${CH}/last.all ${CH}/token.all ${CH}/last.ban ${CH}/token.ban|perl show_p.pl > ../../buff/chinese_author.less
